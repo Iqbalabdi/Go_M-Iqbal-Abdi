6061258460600463cc1c88f2376cffabd0d08c7d
Iqbalabdi <iqbalabdi07@gmail.com>
2022-03-02 18:30:14 +0700

    feat: adding no2 and capture

--- 7_String - Advance Function - Pointer - Method - Struct and Interface/praktikum/2_Caesar Cipher/main.go
+++ 7_String - Advance Function - Pointer - Method - Struct and Interface/praktikum/2_Caesar Cipher/main.go
@@ -5,26 +5,16 @@ import (
 )
 
 func caesar(offset int, input string) string {
-
-	shift, asci := rune(offset), rune(26)
-	// len:=len(input)
-	// newmsg:=""
-	// input=strings.ToUpper(input)
-
-	runes := []rune(input)
-	for index, char := range runes {
-		if char >= 'a'+shift && char <= 'z' || char >= 'A'+shift && char <= 'Z' {
-			char = char - shift
-		} else if char >= 'a' && char < 'a'+shift || char >= 'A' && char < 'A'+shift {
-			char = char - shift + asci
-		}
-
-		runes[index] = char
+	newmsg:=""
+	for i:=0;i<len(input);i++ {
+		newmsg += string((int(input[i]) - 'a'+ offset)%26+'a');
 	}
-	return string(runes)
+	return newmsg
 }
 
 func main() {
 	fmt.Println(caesar(3, "abc"))
+	fmt.Println(caesar(3, "def"))
 	fmt.Println(caesar(1000, "abcdefghijklmnopqrstuvwxyz"))
 }
+
