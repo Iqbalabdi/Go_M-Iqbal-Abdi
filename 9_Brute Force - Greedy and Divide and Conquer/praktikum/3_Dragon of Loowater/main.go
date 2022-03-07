152053501073f69564f3c608c72a467da951aad1
Iqbalabdi <iqbalabdi07@gmail.com>
2022-03-07 18:25:12 +0700

    BREAKING CHANGES: adding task 3 w/ ss

--- 9_Brute Force - Greedy and Divide and Conquer/praktikum/3_Dragon of Loowater/main.go
+++ 9_Brute Force - Greedy and Divide and Conquer/praktikum/3_Dragon of Loowater/main.go
@@ -9,15 +9,32 @@ func DragonOfLoowater(dragonHead, knightHeight []int) {
  	// your code here
   	sort.Ints(dragonHead)
  	sort.Ints(knightHeight)
+	var counts int = 0
+	lenKnight := len(knightHeight)
+	lenDragon := len(dragonHead)
 
-	for i := 0; i<len(dragonHead); i++ {
-		for j := 0; j<len(knightHeight); j++ {
+	if (knightHeight[lenKnight-1] < dragonHead[lenDragon-1]){
+		fmt.Println("knight fall")
+		return
+	}
+
+	for i := 0; i<lenDragon; i++ {
+		for j := 0; j<lenKnight; j++ {
+			if (dragonHead == nil) {
+				fmt.Println(counts)
+				return
+			}
 			if knightHeight[j] >= dragonHead[i] {
-				
+				if dragonHead[i] == 0 {
+					return
+				}
+				counts += knightHeight[j]
+				dragonHead[0] = 0
+				break
 			}
 		}
 	}
-
+	fmt.Println(counts)
 }
 
 func main() {
